ecc28266e0448811b4a9f1896cce76e5791d06e8
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2023-09-04 15:03:07 -0500

    updated names

--- app/locations/[id]/page.tsx
+++ app/locations/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { fetchDataById } from "@/app/utils/fetchData";
 
 export default async function Location({ params }: { params: { id: string } }) {
-  const characterData = await fetchDataById("character", parseInt(params.id));
+  const locationData = await fetchDataById("location", parseInt(params.id));
 
   return (
     <>
       <pre>
-        <code>{JSON.stringify(characterData, null, 2)}</code>
+        <code>{JSON.stringify(locationData, null, 2)}</code>
       </pre>
     </>
   );
