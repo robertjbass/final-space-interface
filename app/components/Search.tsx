a798acfabb49f3d994fe0f5a458acabb2878f09a
Monique <skaiwilliams85@gmail.com>
2023-09-05 00:07:21 -0500

    updated navbar

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -2,13 +2,13 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const Search = () => {
   return (
-    <form className="grid place-items-center md:w-[220px] lg:w-[280px] relative">
+    <form className="grid place-items-center w-[90%] md-2:max-w-[250px] relative">
       <input
         type="text"
         placeholder="Search"
-        className="rounded-md border border-slate-300 py-[0.35rem] px-2 placeholder:font-light focus:outline-none focus:ring-2 focus:ring-slate-200 focus:border-transparent w-[90%] font-light md:w-[100%]"
+        className="rounded-md border border-slate-300 py-[0.35rem] px-2 placeholder:font-light focus:outline-none focus:ring-2 focus:ring-slate-200 focus:border-transparent w-[90%] font-light md-2:w-[100%]"
       />
-      <MagnifyingGlassIcon className="absolute h-5 w-5 text-slate-400 right-3" />
+      <MagnifyingGlassIcon className="absolute h-5 w-5 text-slate-400 right-6 xs:right-8 sm:right-12  md-2:right-3" />
     </form>
   );
 };
