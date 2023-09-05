bb7b5d89afe4e35ce98267ae3e4e1cf6d199b2ad
Monique <skaiwilliams85@gmail.com>
2023-09-05 12:10:41 -0500

    new placeholder

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -5,7 +5,7 @@ const Search = () => {
     <form className="grid place-items-center w-[90%] md-2:max-w-[250px] relative">
       <input
         type="text"
-        placeholder="Search"
+        placeholder="Search final space"
         className="rounded-md border border-slate-300 py-[0.35rem] px-2 placeholder:font-light focus:outline-none focus:ring-2 focus:ring-slate-200 focus:border-transparent w-[90%] font-light md-2:w-[100%]"
       />
       <MagnifyingGlassIcon className="absolute h-5 w-5 text-slate-400 right-6 xs:right-8 sm:right-12  md-2:right-3" />
