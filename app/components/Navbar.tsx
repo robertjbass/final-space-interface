85cb1a56fc61a0ca1d6acfd2d8c16d39c3509a8b
Monique <skaiwilliams85@gmail.com>
2023-09-05 08:11:01 -0500

    Revert "added search bar to nav"
    This reverts commit f1b5fdfa863205bcdbc897f37c2f700563436986.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Menu from "@/app/components/Menu";
-import Search from "@/app/components/Search";
 import Container from "@/app/components/ui/Container";
 import { Bars4Icon } from "@heroicons/react/24/outline";
 import Image from "next/image";
@@ -19,9 +18,9 @@ export default function Navbar() {
     <>
       <nav
         // style={{ height: navbarHeight }}
-        className="bg-white flex items-center border-b-[1px] border-slate-300 h-[12vh] fixed top-0 left-0 w-full z-50"
+        className="bg-white flex items-center border-b-[1px] border-slate-300 h-[10vh]"
       >
-        <Container className="flex justify-between items-center w-full gap-4">
+        <Container className="flex justify-between items-center w-full">
           <Image
             src="/assets/final-space-logo.png"
             width={60}
@@ -29,55 +28,36 @@ export default function Navbar() {
             alt="Logo"
           />
 
-          <div className="flex w-full justify-between items-center max-w-4xl">
-            <Search />
-
-            <ul className="gap-4 hidden md:flex md:-order-1 text-slate-500  font-light">
-              <li>
-                <Link
-                  href="/"
-                  className="hover:underline hover:text-slate-700 "
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/characters"
-                  className="hover:underline hover:text-slate-700 "
-                >
-                  Characters
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/episodes"
-                  className="hover:underline hover:text-slate-700 "
-                >
-                  Episodes
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/locations"
-                  className="hover:underline hover:text-slate-700 "
-                >
-                  Locations
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/quotes"
-                  className="hover:underline hover:text-slate-700 "
-                >
-                  Quotes
-                </Link>
-              </li>
-            </ul>
-          </div>
+          <ul className="gap-4 hidden xs:flex">
+            <li>
+              <Link href="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/characters" className="hover:underline">
+                Characters
+              </Link>
+            </li>
+            <li>
+              <Link href="/episodes" className="hover:underline">
+                Episodes
+              </Link>
+            </li>
+            <li>
+              <Link href="/locations" className="hover:underline">
+                Locations
+              </Link>
+            </li>
+            <li>
+              <Link href="/quotes" className="hover:underline">
+                Quotes
+              </Link>
+            </li>
+          </ul>
 
           <Bars4Icon
-            className="h-6 w-6 cursor-pointer md:hidden"
+            className="h-6 w-6 cursor-pointer xs:hidden"
             onClick={toggleMenu}
           />
         </Container>
